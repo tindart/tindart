428ddc690eb4f59e07c6f7504ab65e6ab858f17e
letsleaveitempty <anita.jaszewska@gmail.com>
2016-04-16 07:03:12 +0200

    fixed includes on array

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -2,7 +2,7 @@ class ArtPiecesController < ApplicationController
   before_action :authenticate_user!
 
   def show_art_piece
-    if current_user.art_pieces.count > 10 #TODO
+    if current_user.art_pieces.count > 2 #TODO
       redirect_to your_match_path
     else
       @art_piece = current_user.unrated_art_pieces.sample
@@ -20,7 +20,7 @@ class ArtPiecesController < ApplicationController
   end
 
   def match_place_and_user
-    top_tag  = calculate_top_tags(1)
+    top_tag    = calculate_top_tags(1)
     top_tag_id = Tag.where(name: top_tag).first.id
 
     @location = set_location(top_tag_id)
@@ -33,20 +33,21 @@ class ArtPiecesController < ApplicationController
 
     def calculate_top_tags(number_of_tags)
       art_pieces_ids = current_user.events.where(event_type: 'like').pluck(:art_piece_id)
-      tags = ArtPiece.where(id: art_pieces_ids).includes(:tags).pluck(:name)
-      tags_counts = Hash.new(0)
+      tags           = ArtPiece.where(id: art_pieces_ids).includes(:tags).pluck(:name)
+      tags_counts    = Hash.new(0)
       tags.each { |tag| tags_counts[tag] += 1 }
       tags_counts.sort_by { |tag, count| count }.reverse.first(number_of_tags).to_h.keys
+
     end
 
     def set_location(tag_id)
       wspolczesne = [2, 8, 15, 16, 21, 26, 27, 28, 39]
-      narodowe = [4, 5, 11, 13, 19, 23, 25, 29, 30, 31, 33, 35, 37, 38]
-      asp = [1, 3, 6, 7, 9, 10, 12, 17, 18, 22, 24]
+      narodowe    = [4, 5, 11, 13, 19, 23, 25, 29, 30, 31, 33, 35, 37, 38]
+      asp         = [1, 3, 6, 7, 9, 10, 12, 17, 18, 22, 24]
 
-      if wspolczesne.includes?(tag_id)
+      if wspolczesne.include?(tag_id)
         Location.find(1)
-      elsif narodowe.includes?(tag_id)
+      elsif narodowe.include?(tag_id)
         Location.find(3)
       else
         Location.find(2)
